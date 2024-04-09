b53f25999a36fb876fbbbfad3380bb704cd19d43
Philipp Roshchin <r.phil@yandex.ru>
2024-04-09 20:58:08 +0300

    new content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,45 @@
 import "./styles/App.css"
-import "./styles/theme.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { useState } from "react"
+import { Button, Row, Form } from 'react-bootstrap'
+import { HttpMethod } from "@data/enums"
+import Fetch from "@API/Fetch"
 
 export default function App() {
 
+    var [predict, setPredict] = useState(0)
+
+    async function getPredict() {
+        var data = await Fetch({ action: `api/predict/`, method: HttpMethod.GET })
+
+        if (data && data.ok) {
+            console.log(data)
+            setPredict(data.predict)
+        }
+        setPredict(Math.random())
+    }
+
     return (
         <div className="App">
-            Hello
+            <br />
+            <h2>Сгенерить предикт на следующий день</h2>
+            <br />
+
+            <Form.Control type="date" />
+
+            <br />
+
+            <Button onClick={getPredict}>клик</Button>
+
+            <br />
+            <br />
+
+            {predict > 0 &&
+                <Row className="Row green text-large">
+                    {predict}
+                </Row>
+            }
         </div>
     )
-
 }
