494c9a975ad6b3e38da7a79bceea715fe56724ac
Alan <tyujn24@mail.ru>
2024-04-09 20:56:12 +0300

    add swagger under nginx

--- app/main.py
+++ app/main.py
@@ -117,6 +117,13 @@ def show_about():
     }
 
 
+@app.post(
+    "/api/v1/test",
+)
+def test(data: dict):
+    return data
+
+
 if __name__ == "__main__":
     uvicorn.run(
         "main:app", host="0.0.0.0", port=8080, reload=True, log_config="log.ini"
