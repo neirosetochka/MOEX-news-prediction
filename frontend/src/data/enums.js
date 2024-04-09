0259fffc0c3b4fc1a24042b0fc11eef0c19e3d4d
Philipp Roshchin <r.phil@yandex.ru>
2024-04-09 16:15:38 +0300

    added bun.lockb & refactoring & removed unused files

---
+++ frontend/src/data/enums.js
@@ -0,0 +1,12 @@
+export var HttpMethod = {
+    GET: "GET",
+    POST: "POST",
+    PUT: "PUT",
+    DELETE: "DELETE"
+}
+
+export var Theme = {
+    NAME: "theme",
+    LIGHT: "light",
+    DARK: "dark"
+}
