c9fd4358d890961cf906b57bb857610687d20c7b
Philipp Roshchin <r.phil@yandex.ru>
2024-04-11 19:32:05 +0300

    refactoring & new plot

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,129 @@
 import "./styles/App.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 
-import Predict from "components/Predict"
-import ModelPlot from "components/ModelPlot"
+import { useState } from "react"
+import { Row } from "react-bootstrap"
+import Plot from "react-plotly.js"
+
+import { HttpMethod } from "@data/enums"
+import Fetch from "@API/Fetch"
+import PredictForm from "components/PredictForm"
 
 
 export default function App() {
+
+    var [error, setError] = useState("")
+    var [success, setSuccess] = useState(false)
+
+    var [predict, setPredict] = useState({
+        dates: [],
+        x: [],
+        y_pred: [],
+        y_true: []
+    })
+
+    async function getPredict(date) {
+        var body = { date: date }
+        var response = await Fetch({ action: "api/v1/predict/", method: HttpMethod.POST, body: body })
+
+        // TODO: remove
+        response = {
+            data: {
+                dates: ["2020-01-01", "2020-01-02", "2020-01-03", "2020-01-04", "2020-01-05", "2020-01-06"],
+                x: [1, 2, 3, 4, 5, 6],
+                y_pred: [1, 2, 3, 1, 2, 3],
+                y_true: [2, 3, 1, 2, 4, 1]
+            }
+        }
+
+        if (response && response.data) {
+            setError("")
+            setSuccess(true)
+            setPredict(response.data)
+        } else {
+            setSuccess(false)
+            setError("Error")
+            setPredict({
+                dates: [],
+                x: [],
+                y_pred: [],
+                y_true: []
+            })
+        }
+    }
+
     return (
         <div className="App">
+            <br />
+            <h2 className="text-center">Команда А</h2>
+            <br />
+
+            <PredictForm
+                getPredict={getPredict}
+                error={error}
+                setError={setError}
+                success={success}
+                setSuccess={setSuccess} />
 
             <br />
 
-            <Predict />
+            {error &&
+                <Row className="Row text-large text-red">
+                    {error}
+                </Row>
+            }
 
             <br />
             <br />
-            <br />
 
-            <ModelPlot />
+            {predict.x.length > 0
+                &&
+
+                <Plot
+                    data={[
+                        {
+                            x: predict.dates,
+                            // x: ["2020-01-01", "2020-01-02", "2020-01-03", "2020-01-04", "2020-01-05", "2020-01-06"],
+                            y: predict.y_true,
+
+                            type: "scatter",
+                            mode: "lines+markers",
+                            marker: { color: "blue" },
+                            name: "истинные",
+                        },
+                        {
+                            x: predict.dates,
+                            // x: ["2020-01-01", "2020-01-02", "2020-01-03"],
+                            y: predict.y_pred,
+
+                            type: "scatter",
+                            mode: "lines+markers",
+                            marker: { color: "orange" },
+                            name: "предсказанные",
+                        },
+                    ]}
+                    layout={{
+                        title: `предсказания с ${predict.dates[0]} по ${predict.dates[predict.dates.length - 1]}`,
+                        responsive: true,
+                        useResizeHandler: true,
+                        autosize: true,
+                        // margin: { "l": 0, "r": 0, "b": 0, "t": 0 },
+                        xaxis: {
+                            title: "дата",
+                            type: "category",
+                            zeroline: false,
+                            showline: false,
+                        },
+                        yaxis: {
+                            title: "стоимость",
+                            zeroline: false,
+                            showline: false,
+                            // range: [-10, 100],
+                        },
+                    }}
+                    style={{ width: "100%", height: 600 }}
+                />
+            }
         </div>
     )
 }
--- frontend/src/components/PredictForm.jsx
+++ frontend/src/components/PredictForm.jsx
@@ -1,40 +1,59 @@
-import { useState } from "react"
-import { Button, Form, InputGroup } from "react-bootstrap"
+import { useEffect, useState } from "react"
+import { Button, Form } from "react-bootstrap"
 
-export default function PredictForm({ getPredict, setPredict, setError }) {
 
-    var [date, setDate] = useState("")
+export default function PredictForm({ getPredict, error, setError, success, setSuccess }) {
+
+    var [dateInterval, setDateInterval] = useState({ start: "", end: "" })
+    var [buttonColor, setButtonColor] = useState("secondary")
 
     async function callFunc(event) {
         await event.preventDefault()
-        if (date) {
+        if (dateInterval.start && dateInterval.end) {
             setError("")
-            return await getPredict(date)
+            return await getPredict(dateInterval)
         } else {
-            setPredict("")
-            setError("Write date")
+            setError("введите дату")
         }
+        setSuccess(false)
     }
 
+    useEffect(() => {
+        var buttonNewColor = ""
+
+        if (error === "" && success === false) {
+            buttonNewColor = "secondary"
+        } else if (error !== "") {
+            buttonNewColor = "danger"
+        } else if (success === true) {
+            buttonNewColor = "success"
+        }
+        setButtonColor(buttonNewColor)
+
+    }, [error, success])
+
     return (
-        <>
-            <Form formAction="POST" onSubmit={e => callFunc(e)}>
-                <Form.Group className="mb-3">
-                    <InputGroup className="mb-3">
-                        <Form.Control
-                            type="date"
-                            onChange={e => setDate((prev) => e.target.value)}
-                        />
-                        <Button
-                            variant="outline-secondary"
-                            id="button-addon1"
-                            type="submit"
-                        >
-                            get predict
-                        </Button>
-                    </InputGroup>
-                </Form.Group>
-            </Form>
-        </>
+        <Form formAction="POST" onSubmit={e => callFunc(e)}>
+            <Form.Group className="mb-3">
+                <Form.Label>начальная дата</Form.Label>
+                <Form.Control
+                    type="date"
+                    onChange={e => setDateInterval({ ...dateInterval, start: e.target.value })}
+                />
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>конечная дата</Form.Label>
+                <Form.Control
+                    type="date"
+                    onChange={e => setDateInterval({ ...dateInterval, end: e.target.value })}
+                />
+            </Form.Group>
+
+            <div className="d-grid gap-2">
+                <Button variant={buttonColor} type="submit">
+                    предсказать
+                </Button>
+            </div>
+        </Form>
     )
 }
